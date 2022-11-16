75d911ab25c2dff07daa911befaa663e84de813b
uoc <uoc@lexorit.com>
2022-11-16 17:27:21 +0700

    .

---
+++ src/main/java/com/myteam/travel/db/DBSource.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.myteam.travel.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author uoc
+ */
+public class DBSource {
+    private static String dbURL = "jdbc:mysql://103.130.213.10:3306/TravelApp";
+    private static String userName = "cxluser";
+    private static String password ="Thanh0974135042!";
+    
+    public DBSource() {
+
+    }
+
+    public static Connection getConnection() {
+        Connection conn = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            conn = DriverManager.getConnection(dbURL, userName, password);
+            System.out.println("connect successfully!");
+        } catch (Exception ex) {
+            System.out.println("connect failure!");
+            ex.printStackTrace();
+        }
+        return conn;
+    }
+}
