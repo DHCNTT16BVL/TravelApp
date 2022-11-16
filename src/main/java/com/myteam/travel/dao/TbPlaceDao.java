67c6a9378c86e7ccd0b9ca1f89f0ff75b11233f4
uoc <pminhuoc92@gmail.com>
2022-11-17 00:02:35 +0700

    .

--- src/main/java/com/myteam/travel/dao/TbPlaceDao.java
+++ src/main/java/com/myteam/travel/dao/TbPlaceDao.java
@@ -4,10 +4,28 @@
  */
 package com.myteam.travel.dao;
 
+import com.myteam.travel.db.DBSource;
+import com.myteam.travel.model.TbPlace;
+import java.sql.ResultSet;
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  *
  * @author uoc
  */
 public class TbPlaceDao {
-    
+
+    public List<TbPlace> list() {
+        List<TbPlace> list = new LinkedList<>();
+        String sql = "select * from tbPlace";
+        ResultSet rs;
+        try {
+            rs = DBSource.runQuery(sql);
+        } catch (Exception e) {
+        } finally {
+            //rs.close();
+        }
+        return list;
+    }
 }
